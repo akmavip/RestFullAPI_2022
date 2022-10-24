aad8f797fd80a4e951612635769d41d72d5c18a0
VTD <vutanduc57200@gmail.com>
2022-10-25 00:11:31 +0700

    demo merge

--- src/main/java/com/restfull/oop/service/TaiKhoanService.java
+++ src/main/java/com/restfull/oop/service/TaiKhoanService.java
@@ -6,9 +6,14 @@ import com.restfull.oop.vm.TaiKhoanVM;
 import java.util.List;
 
 public interface TaiKhoanService {
+
     List<TaiKhoanVM> getList();
+
     TaiKhoanVM getDetail(Long maTk);
+
     TaiKhoanVM create(TaiKhoanDTO taiKhoanDTO);
+
     TaiKhoanVM update(TaiKhoanDTO taiKhoanDTO);
+
     void delete(Long maTK);
 }
